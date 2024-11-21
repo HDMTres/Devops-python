ce50a27cf72b9967bdcfb55578769be9b5803048
HDMTres <toure.hdmpro@gmail.com>
2024-11-22 00:24:59 +0100

    test2

--- health-calculator-service/test.py
+++ health-calculator-service/test.py
@@ -4,7 +4,7 @@ from health_utils import calculate_bmi, calculate_bmr
 class TestHealthUtils(unittest.TestCase):
 
     def test_calculate_bmi(self):
-        self.assertAlmostEqual(calculate_bmi(1.75, 70), 0.0, places=2)
+        self.assertAlmostEqual(calculate_bmi(1.75, 70), 22.86, places=2)
     
     def test_calculate_bmr_male(self):
         self.assertAlmostEqual(calculate_bmr(175, 70, 25, 'male'), 2626.84, places=2)
